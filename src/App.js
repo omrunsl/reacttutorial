6ae3672e38946b31da2be3e4660ed32123ad16f7
Ömer Ünsal <omer.unsal@timurlar.local>
2019-05-20 17:37:19 +0300

    second commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar'
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="container">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
